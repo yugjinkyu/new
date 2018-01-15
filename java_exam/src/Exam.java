6ba7ce1b7158c2d82ef641dbda856a4de7bbc8bc
401-04-PC\401-04 <yugjinkyu@gmail.com>
2018-01-15 12:35:08 +0900

    1/15

---
+++ java_exam/src/Exam.java
@@ -0,0 +1,48 @@
+/*public class Exam
+{
+public static void main(String[] args) {
+  System.out.println("Fun Programing");
+   System.out.println("Programing! It's fun.");
+   System.out.println("My name is Hong Gil Dong");
+   System.out.println("I am 13 years old");
+   System.out.println("(@) (@)");
+   System.out.println("(=^.^=)");
+   System.out.println("(-m-m-");
+   System.out.println("I can program well.");
+   System.out.println("My height");
+   System.out.println(170);
+   System.out.println("My weight");
+   System.out.println("68.60000");
+   System.out.println("5 Dan");
+   System.out.println("5*2=10");
+   System.out.println("item count price");
+   System.out.println("pen 20 100");
+   System.out.println("note 5 95");
+   System.out.println("eraser 110 97");
+   System.out.println("My name is Hong");
+   System.out.println("My hometown");
+   System.out.println("Flowering mountain");
+
+}
+*/
+
+
+
+
+
+class Exam
+{
+    public static void main(String[] args)
+    {
+    int num1;
+    num1=10;
+
+    int num2=20;
+    int num3=num1+num2;
+    System.out.println(num1+"+"+num2+"="+num3);
+    }
+}
+
+
+
+
